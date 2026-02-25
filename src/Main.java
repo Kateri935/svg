4f4ca541229b59f6e3daa3fd3b8a5e66cffeead9
student <anton2karolik@gmail.com>
2026-02-25 19:21:39 +0100

    zadanie 1

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//        }
+        Point point = new Point();
+        System.out.println(point.x + " " + point.y);
+        point.x =7;
+        System.out.println(point.x + " " + point.y);
+        System.out.println(point.toString());
     }
 }
